e795b33f6eae3af58659f0ddab742a04d6acea37
DangTrieu02 <dangtrieu.cdht@gmail.com>
2023-03-06 08:36:58 +0700

    aaaaaaaaaaaaaaaa

---
+++ dist/src/controllers/walletController.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const walletSchema_1 = require("../models/schemas/walletSchema");
+const user = walletSchema_1.Wallet;
+class WalletController {
+    constructor() { }
+}
+//# sourceMappingURL=walletController.js.map
