75d764b72bb1929827a6add9cf2747dfdc724736
quang <ltb01628645546@gmail.com>
2023-03-06 20:48:32 +0700

    doneaa

--- dist/src/routers/listWallerRouter.js
+++ dist/src/routers/listWallerRouter.js
@@ -6,6 +6,7 @@ const wallerSchema_1 = require("../models/schemas/wallerSchema");
 productRouters.get('/listWaller', async (req, res) => {
     try {
         const products = await wallerSchema_1.Product.find();
+        console.log("abc", products);
         res.render("listWaller", { products: products });
     }
     catch (_a) {
--- src/routers/listWallerRouter.ts
+++ src/routers/listWallerRouter.ts
@@ -5,7 +5,9 @@ import {Product} from "../models/schemas/wallerSchema";
 productRouters.get('/listWaller', async (req, res)=>{
     try {
         const products = await Product.find();
+        console.log("abc", products);
         res.render("listWaller", {products: products});
+
     }catch {
         res.render("error");
     }
